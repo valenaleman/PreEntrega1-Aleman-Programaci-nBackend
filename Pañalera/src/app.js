d521ec85d2320a8965065c7fb2d9db2abcfb3dac
Valentina Lujan Aleman <valentinaaleman07@gmial.com>
2023-11-07 21:10:42 -0300

    agrego cambios

--- Pañalera/src/app.js
+++ Pañalera/src/app.js
@@ -3,21 +3,10 @@ import express from 'express';
 const app = express();
 const port = 8080;
 
-app.get('/success', (req, res) => {
-    res.status(200).send('Solicitud exitosa');
-});
-
-app.get('/persona', (req, res) => {
-    const persona = {
-        nombre: 'Valen',
-        apellido: 'A.',
-        edad: 20,
-        correo: 'valentinaaleman07@gmail.com'
-    };
-
-    res.status(200).json(persona);
-});
-
 app.listen(port, () => {
     console.log('El servidor express está escuchando en el puerto ${port}');
+});
+
+app.get('/success', (req, res) => {
+    res.status(400).send('Solicitud exitosa');
 });
